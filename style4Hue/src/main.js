a69d9b4772c9137ff598793b1719184447a6f91b
AJ Sigsbury <asigsbur@uci.edu>
2023-05-27 14:39:31 -0700

    Added home -> main.js

--- style4Hue/src/main.js
+++ style4Hue/src/main.js
@@ -1,8 +1,17 @@
 import './app.css'
-import App from './App.svelte'
+//import App from './App.svelte'
+import Home from './Home.svelte'
 
+
+/*
 const app = new App({
   target: document.getElementById('app'),
 })
+*/
 
-export default app
+const home = new Home({
+  target: document.getElementById('home'),
+})
+
+//export default app
+export default home
