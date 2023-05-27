c9b723e74c302fe5073ad5ddb18e0eecbad215ec
scottphamm <scotttp1@uci.edu>
2023-05-27 13:57:48 -0700

    Created Svelte frontend.

---
+++ src/MyComponent.svelte
@@ -0,0 +1,34 @@
+<script>
+    
+    function handlePhotoUpload(event){
+        const photo = event.target.files[0];
+
+        console.log("Uploaded photos:", photo)
+    }
+    
+    // function selectPhoto() {}
+
+    // function submitSelections() {}
+
+   let files;
+
+    $: if (files) {
+        // Note that `files` is of type `FileList`, not an Array:
+        // https://developer.mozilla.org/en-US/docs/Web/API/FileList
+        console.log(files);
+
+        for (const file of files) {
+            console.log(`${file.name}: ${file.size} bytes`);
+        }
+    
+</script>
+
+<label for="many">Upload multiple photos:</label>
+<input
+	bind:photos
+	id="many"
+	multiple
+	type="photo"
+/>
+
+
--- src/uploader.py
+++ src/uploader.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return 'Welcome to the Flask backend!'
+
+@app.route('/api/data')
+def get_data():
+    data = {'message': 'Data from the Flask backend!'}
+    return data
 
 if __name__ == '__main__':
     app.run()
