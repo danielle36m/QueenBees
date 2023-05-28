b85337382fd5496df0e684555b9fcc63cd4a0e7a
AJ Sigsbury <asigsbur@uci.edu>
2023-05-27 22:52:30 -0700

    Simplified code organization/styles

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -15,11 +15,11 @@
     
 <div class="root">
     <div class="pinkrectangle">
-        <div class="topBar">
-            <div class="headerName">
-                StyleForHue
-            </div>
+        <!-- <div class="topBar"> -->
+        <div class="headerName">
+            StyleForHue
         </div>
+        <!-- </div> -->
 
         <h2>
             Upload Your Wardrobe
@@ -42,84 +42,60 @@
         {/each}
         {/if}
         
-        <!-- <button on:click>
-        Click me
-        </button> -->
+        <button on:click>
+        Submit Photos
+        </button>
     </div>
 </div>
     
 <style>
-    /* button {
-        position: relative;
-        left: 15.8%;
+    button {
+        /* left: 10.8%; */
         height: 3rem;
         width: 6rem;
-        background-color: #aaa;
-        border-color: #f1c40f;
-        color: #f1c40f;
+        background-color: #ffffff;
+        border-color: #dd7729;
+        color: #000000;
         font-size: 1.15rem;
-        background-image: linear-gradient(45deg, #f1c40f 50%, transparent 50%);
+        background-image: linear-gradient(45deg, #ef8635 50%, transparent 50%);
         background-position: 100%;
         background-size: 400%;
         transition: background 300ms ease-in-out;
     }
     button:hover {
         background-position: 0;
-        color: #aaa;
-    } */
+        color: #ffffff;
+    }
     .root {
-        
         width: 100%;
-        height: 1438px;
-        align-items: flex-start;
+        height: 800px;
         background-color: #fff;
-        overflow: hidden;
     }
     .pinkrectangle {
-       
-        left: 0;
-        top: 0;
-        width: 100%;
         height: 9%;
         background-color: #ffcfd4;
     }
-    .topBar {
-        
-        height: min-content;
-        place-content: space-between;
-        align-items: center;
-    }
+    /* .topBar {
+        top: 10%;
+        left: 5%;
+    } */
     .headerName {
         color: #fff7f7;
         font-size: 40px;
-        line-height: 125%;
+        line-height: 175%;
         font-weight: 500;
         font-family: Work Sans;
-        width: min-content;
-        height: min-content;
-        white-space: nowrap;
-        flex-direction: column;
+        left: 5%;
+        padding: 0 .75em;
     }
     h2 {
-
-        color: rgb(220,113,43);
+        color: rgb(220, 113, 43);
         text-align: center;
         padding: 1.5em 0.5em 0.5em 0.5em;
-        box-sizing: border-box;
-        pointer-events: none;
-        justify-content: center;
-        align-items: center;
-        padding: 60px;
     }
     h5 {
-
         color: rgb(220,113,43);
         text-align: center;
-        box-sizing: border-box;
-        pointer-events: none;
-        justify-content: center;
-        align-items: center;
-        padding: 70px;
     }
     .image-size {
         width: 200px;
