3f67afc8ffac31f352c85d3a34c5faa20d3c447c
AJ Sigsbury <asigsbur@uci.edu>
2023-05-27 17:05:06 -0700

    Revert "Added home -> main.js"
    This reverts commit a69d9b4772c9137ff598793b1719184447a6f91b.

--- style4Hue/src/main.js
+++ style4Hue/src/main.js
@@ -1,17 +1,8 @@
 import './app.css'
-//import App from './App.svelte'
-import Home from './Home.svelte'
+import App from './App.svelte'
 
-
-/*
 const app = new App({
   target: document.getElementById('app'),
 })
-*/
 
-const home = new Home({
-  target: document.getElementById('home'),
-})
-
-//export default app
-export default home
+export default app
