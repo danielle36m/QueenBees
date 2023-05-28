7daec7b9d42708f7bb4d65ec9f483c75c64b8140
AJ Sigsbury <asigsbur@uci.edu>
2023-05-27 20:26:20 -0700

    Created layout of home page

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,29 +1,39 @@
 <script>
     let files;
     
-        $: if (files) {
-            // Note that `files` is of type `FileList`, not an Array:
-            // https://developer.mozilla.org/en-US/docs/Web/API/FileList
-            console.log(files);
-    
-            for (const file of files) {
-                console.log(`${file}`);
-                //console.log(`${file.name}: ${file.size} bytes`);
-            }
+    $: if (files) {
+        // Note that `files` is of type `FileList`, not an Array:
+        // https://developer.mozilla.org/en-US/docs/Web/API/FileList
+        console.log(files);
+
+        for (const file of files) {
+            console.log(`${file}`);
+            //console.log(`${file.name}: ${file.size} bytes`);
         }
-    </script>	
+    }
+  </script>
+
     
     <div class="root">
-        <div class="rectangle">
+        <div class="pinkrectangle">
+        <div class="topBar">
+            <div class="barTopLeft">
+                <div class="headerName">
+                    StyleForHue
+                </div>
+            </div>
+        </div>
+
         <h2>
             Upload Your Wardrobe!
         </h2>
-        <button on:click>
+        
+        <!-- <button on:click>
         Click me
-    </button>
+        </button> -->
         </div>
     
-        <div class="rectangle2">
+        <div class="greenrectangle">
             
         </div>
     </div>
@@ -50,37 +60,64 @@
         .root {
             position: relative;
             width: 1440px;
-            height: 540px;
+            height: 1439px;
             align-items: flex-start;
             background-color: #fff;
             overflow: hidden;
         }
         
-        .rectangle {
+        .pinkrectangle {
             position: absoulte;
             left: 0;
             top: 0;
             width: 1440px;
-            height: 200px;
+            height: 658px;
             background-color: #ffcfd4;
         }
-        .rectangle2 {
+        .greenrectangle {
             position: absolute;
             left: 0;
-            top: 440px;
+            top: 1301px;
             width: 1440px;
-            height: 100px;
+            height: 138px;
             background-color: #afd1a0;
         }
         
         h2 {
-            //position: relative;
-            width: 38.5%;
-            //height: 300vh;
+            width: 100%;
             color: rgb(220,113,43);
             text-align: center;
             padding: 1.5em 0.5em 0.5em 0.5em;
             box-sizing: border-box;
             pointer-events: none;
+            justify-content: center;
+		    align-items: center;
+		    padding: 12px;
+        }
+        .topBar {
+            position: absolute;
+            left: 135px;
+            top: 66px;
+            width: 1175px;
+            height: min-content;
+            place-content: space-between;
+            align-items: center;
+        }
+        .barTopLeft {
+            width: min-content;
+            height: min-content;
+            align-items: center;
+            gap: 18px;
+        }
+        .headerName {
+            color: #fff7f7;
+            font-size: 40px;
+            line-height: 125%;
+            font-weight: 500;
+            font-family: Work Sans;
+            width: min-content;
+            height: min-content;
+            white-space: nowrap;
+            flex-direction: column;
         }
     </style>
